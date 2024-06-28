62ffc1919eed8b7224f45a141e2337bbac7b1e2a
Rushil Jalal <rushiljalal@gmail.com>
2024-06-28 19:37:56 +0530

    another fix

--- auth.config.ts
+++ auth.config.ts
@@ -14,25 +14,7 @@ export const authConfig = {
      * Used by MiddleWare
      */
     async authorized({ auth, request }) {
-      const // Check if the user is authenticated
-        isUserAuthenticated = !!auth?.user,
-        // Initialize protected routes
-        isAuthRequired = !request.nextUrl.pathname.startsWith(
-          "https://rushil-ecommerce-store.vercel.app/api/auth/signin?callbackUrl=https://rushil-ecommerce-store.vercel.app/api/auth/callback/github"
-        );
-
-      // If the route is protected
-      return isAuthRequired
-        ? // Check if the user is authenticated; return true if the user is authenticated, false otherwise
-          isUserAuthenticated
-          ? true
-          : false
-        : // If the route is not protected
-        isUserAuthenticated && request.nextUrl.pathname.startsWith("/login")
-        ? // and the user is authenticated, redirect to the specified page
-          Response.redirect(new URL("/", request.nextUrl).toString())
-        : // and the user is not authenticated, return true
-          true;
+      return !!auth?.user;
     },
   },
 } satisfies NextAuthConfig;
