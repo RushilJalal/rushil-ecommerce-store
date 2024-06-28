b53fd4da63b6bcbe00ec7fd4f45c4c239fc149ed
Rushil Jalal <rushiljalal@gmail.com>
2024-06-28 18:43:08 +0530

    new changes

--- auth.ts
+++ auth.ts
@@ -5,10 +5,26 @@ import Github from "next-auth/providers/github";
 export const config = {
   providers: [Github],
   callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-      if (pathname === "./middleware.ts") return !!auth;
-      return true;
+    async authorized({ auth, request }) {
+      const // Check if the user is authenticated
+        isUserAuthenticated = !!auth?.user,
+        // Initialize protected routes
+        isAuthRequired = !request.nextUrl.pathname.startsWith(
+          "https://rushil-ecommerce-store.vercel.app/api/auth/signin?callbackUrl=https://rushil-ecommerce-store.vercel.app/api/auth/callback/github"
+        );
+
+      // If the route is protected
+      return isAuthRequired
+        ? // Check if the user is authenticated; return true if the user is authenticated, false otherwise
+          isUserAuthenticated
+          ? true
+          : false
+        : // If the route is not protected
+        isUserAuthenticated && request.nextUrl.pathname.startsWith("/login")
+        ? // and the user is authenticated, redirect to the specified page
+          Response.redirect(new URL("/", request.nextUrl).toString())
+        : // and the user is not authenticated, return true
+          true;
     },
     jwt({ token, user, profile }) {
       if (user && profile) {
